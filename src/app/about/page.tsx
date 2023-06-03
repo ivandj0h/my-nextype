bb45756a42901cd6849e176e236a3be0520553bd
ivandjoh <ivandi.djoh@gmail.com>
2023-06-04 05:05:08 +0800

    add styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,16 +1,15 @@
 "use client"
 
 import React from "react";
-import {container, mainStyle, textLg} from "@/utils/styles-collections";
 
 
 const AboutPage: React.FC = () => {
     return (
-        <main className={mainStyle}>
-            <div className={container}>
-                <h1 className={[textLg, "pt-8"].join(" ")}>This is About Page</h1>
+        <>
+            <div>
+                <h1>This is About Page</h1>
             </div>
-        </main>
+        </>
     );
 }
 
--- src/utils/styles-collections/global_styles.ts
+++
@@ -1,9 +0,0 @@
-const mainStyle = ["flex"].join(" ");
-const container = ["container", "mx-auto", "text-center"].join(" ");
-const textLg = ["text-lg", "font-bold", "text-gray-300"].join(" ");
-
-export {
-    mainStyle,
-    container,
-    textLg
-}
