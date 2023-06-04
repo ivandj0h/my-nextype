c5a8d2f2ac37d688951bc3664236cb2a004b2eb3
ivandjoh <ivandi.djoh@gmail.com>
2023-06-04 10:55:22 +0800

    adjust nav & footer

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 `My NexType` is a small project that I am working on to help me learn how to use the [NexType](https://nextjs.org/) framework. I am also using this project to learn how to use [Tailwind CSS](https://tailwindcss.com/). I am also using this project to learn how to use [TypeScript](https://www.typescriptlang.org/).
 
 ## Screenshots
-![Home](./public/readme/home_img.png)
+![Home](./public/readme/homepage_image.png)
 
 ## Authors
 - [**ivandjoh**](https://linkedin.com/in/ivandjoh)
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,21 +6,22 @@ import Link from "next/link";
 import styles from "./navbar.module.css";
 
 import {links} from "@/utils/nav-links/NavLinks";
-import {navBarContainer} from "@/utils/components-styles/navbarStylesComponent";
 import Image from "next/image";
 
 const Navbar: React.FC = () => {
     return (
         <>
-            <nav className={[navBarContainer, styles.container].join(" ")}>
-                <Link href="/" className={styles.logo}>
-                    <div className="flex items-center">
-                        <Image src="/logo.svg" alt="ivandjoh" width={50} height={50} className="rounded-full" />
-                        <span className="ml-1 font-light">.com</span>
-                    </div>
-                </Link>
+            <nav className={`${styles.navBar} fixed w-full z-50 flex justify-between bg-white border-b border-gray-200 shadow-sm h-24 p-2`}>
+                <div className="flex items-center px-4 sm:px-6 lg:px-8">
+                    <Link href="/" className={styles.logo}>
+                        <div className="flex items-center">
+                            <Image src="/logo.svg" alt="ivandjoh" width={50} height={50} className="rounded-full"/>
+                            <span className="ml-1 font-light">.com</span>
+                        </div>
+                    </Link>
+                </div>
 
-                <div className={styles.links}>
+                <div className={`${styles.links} flex justify-end space-x-4 px-4 sm:px-6 lg:px-8`}>
                     {links.map((link, index) => {
                         return (
                             <Link href={link.url} key={index} className={styles.link}>{link.title}</Link>
@@ -28,7 +29,8 @@ const Navbar: React.FC = () => {
                     })}
                     <button
                         className="bg-sky-500 hover:bg-sky-600 hover:text-white text-white font-medium py-2 px-4 rounded-md max-w-fit"
-                        onClick={() => console.log("clicked")}>Logout</button>
+                        onClick={() => console.log("clicked")}>Logout
+                    </button>
                 </div>
             </nav>
         </>
