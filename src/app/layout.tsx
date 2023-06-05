cb075fe637423e4db853d37381f61281aafa3441
ivandi1980 <ivandi.djoh@gmail.com>
2023-06-05 20:14:08 +0800

    fixed layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,37 @@
 import React from "react";
-import './globals.css'
-import {Montserrat} from 'next/font/google'
-import {config} from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
+import "./globals.css";
+import { Montserrat } from "next/font/google";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
-config.autoAddCss = false
+config.autoAddCss = false;
 
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
 
-
 const montserrat = Montserrat({
-    subsets: ['latin'],
-    display: 'swap',
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata = {
-    title: process.env.NEXT_PUBLIC_METADATA_TITLE || 'ivandjohn.com',
-    description: process.env.NEXT_PUBLIC_METADATA_DESCRIPTION || 'ivandjoh Official Website',
-}
+  title: process.env.NEXT_PUBLIC_METADATA_TITLE || "ivandjohn.com",
+  description:
+    process.env.NEXT_PUBLIC_METADATA_DESCRIPTION || "ivandjoh Official Website",
+};
 
-export default function RootLayout({children}: { children: React.ReactNode }) {
-    return (
-        <html lang="en" className={montserrat.className}>
-        <body className="flex flex-col min-h-screen overflow-hidden">
-        <Navbar/>
-        <div className="container flex-grow">
-            {children}
-        </div>
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={montserrat.className}>
+      <body className="flex flex-col min-h-screen">
+        <Navbar />
+        <div className="container flex-grow custom-scrollbar">{children}</div>
         <Footer className="bg-gray-200 py-4 px-6 fixed bottom-0 w-full flex justify-between border-t border-gray-200 h-24 p-2" />
-        </body>
-        </html>
-    )
+      </body>
+    </html>
+  );
 }
